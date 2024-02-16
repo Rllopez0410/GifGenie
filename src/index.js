177963b3a1a6e274543725716dfee57050ad8547
Roger Lopez <rllopez0410@gmail.com>
2024-02-16 17:37:15 -0500

    created a funtional search bar for users to search their desire gifs

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 import "./style.scss";
+const Dom = {
+    image: document.querySelector("img"),
+    btn: document.querySelector("button"),
+    input: document.querySelector("input")
+}
 
-const image = document.querySelector("img");
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", fetchNew);
 
 let click = false;
 
+Dom.btn.addEventListener("click", fetchNew);
+
 function fetchNew() {
-  click = true;
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=BIZNCFPH907rY4wbnPHeYKQOrfZ9cyOH&s=dogs', {mode: 'cors'})
+    click = true;
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=BIZNCFPH907rY4wbnPHeYKQOrfZ9cyOH&s=' +  input.value, {mode: 'cors'})
 .then(function(response) {
   return response.json();
-})
-  .then(function(response) {
-    image.src = response.data.images.original.url;
+}).then(function(response) {
+    Dom.image.src = response.data.images.original.url;
   });
 }
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=BIZNCFPH907rY4wbnPHeYKQOrfZ9cyOH&s=dogs', {mode: 'cors'}, fetchNew)
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=BIZNCFPH907rY4wbnPHeYKQOrfZ9cyOH&s=hello', {mode: 'cors'}, fetchNew)
 .then(function(response) {
   return response.json();
-})
-  .then(function(response) {
-    image.src = response.data.images.original.url;
+}).then(function(response) {
+    Dom.image.src = response.data.images.original.url;
     if (click) {
       fetchNew();
       click = false;
