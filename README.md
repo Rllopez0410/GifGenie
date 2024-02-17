38330d9b9c54d44f00469a360128639c287857f1
Roger Lopez <rllopez0410@gmail.com>
2024-02-16 22:44:22 -0500

    created a container that creates a clickible link to the gif when fetched

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GifGenie
-Sometimes finding a gif can be difficult; whether you trying to make someone laugh, comfort a friend or even congatulate them on their new accomplishments. GifGenie helps you find the gif you need in little to no time with minimal effort. Simply type in the search bar what you'll like and poof, like magic, GifGenie grants you what you wished for in addition to an availible link to the gif.
+Sometimes finding a gif can be difficult; whether you're trying to make someone laugh, comfort a friend or even congratulate them on their new accomplishments. GifGenie helps you find the gif you need in little to no time with minimal effort. Simply type in the search bar what you'll like and poof, like magic, GifGenie grants you what you wished for in addition to an availible link to the gif.
+
+## Technologies: JS, GIT, Webpack, SCSS, HTML, GIPHY API.
--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import "./style.scss";
 const Dom = {
     image: document.querySelector("img"),
     btn: document.querySelector("button"),
-    input: document.querySelector("input")
+    input: document.querySelector("input"),
+    link: document.querySelector("a")
 }
-
 
 let click = false;
 
-Dom.btn.addEventListener("click", fetchNew);
+Dom.btn.addEventListener("click", fetchNewGif);
 
-function fetchNew() {
+function fetchNewGif() {
     click = true;
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=BIZNCFPH907rY4wbnPHeYKQOrfZ9cyOH&s=' +  input.value, {mode: 'cors'})
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=BIZNCFPH907rY4wbnPHeYKQOrfZ9cyOH&s=' +  Dom.input.value, {mode: 'cors'})
 .then(function(response) {
   return response.json();
 }).then(function(response) {
     Dom.image.src = response.data.images.original.url;
+    Dom.link.download = response.data.images.original.url;
+    Dom.link.textContent = "Click Here!";
+    Dom.input.value = null;
+    console.log(response.data.images.original.url);
   });
 }
 
